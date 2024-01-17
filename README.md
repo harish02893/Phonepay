9e870e5d84f70b2350009272edc64b2ceaf0676e
kesavan Sekar <136705305+harish02893@users.noreply.github.com>
2024-01-18 00:38:42 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Phonepay
+# Phonepe_Pulse_Data_Visualization
+
+# Youtube Video - https://youtu.be/BywYo-UMJwU   
+![image](https://github.com/omkarm0542/Phonepe_Pulse_Data_Visualization/assets/123791884/e0d7bc40-99da-4a49-bd92-c7b65a94671a)
+
+This project retrieves data from the PhonePe Pulse Github repository, transforms it, and stores it in a MySQL database. It then creates an interactive dashboard using Streamlit and Plotly to visualize the data.
+
+![image](https://user-images.githubusercontent.com/123791884/236590536-9d3687ed-4fca-4286-a36f-eb750e323d1c.png)
+
+
+# Prerequisites
+* Python 3.x
+
+* Git
+
+* MySQL
+
+# Installation                                                                             ![image](https://user-images.githubusercontent.com/123791884/236590572-4eb3f26c-5ed4-49f3-9277-e53e02e2c6e4.png)
+
+1. Clone the repository:
+
+> git clone https://github.com/username/repo.git
+
+2. Install the required packages:
+
+> pip install -r requirements.txt
+
+3. Configure the MySQL database by editing the config.ini file with your credentials.
+
+# Usage
+1. Run the main.py script to retrieve, transform, and store the data:
+
+> python main.py
+
+2. Launch the dashboard by running the app.py script:
+
+> streamlit run app.py
+
+3. Select different dropdown options to display various facts and figures on the dashboard.
+
+# License
+
+This project is licensed under the Apache License 2.0. See the LICENSE file for details.
+
+# Acknowledgments
+
+> PhonePe Pulse Github repository
+> Streamlit
+> Plotly
+> MySQL
---
+++ ph.py
@@ -0,0 +1,392 @@
+#pip install mysql-connector-python
+#pip install streamlit plotly mysql-connector-python
+#pip install streamlit
+#pip install streamlit_extras
+
+import mysql.connector 
+import pandas as pd
+#import psycopg2
+import streamlit as st
+import PIL 
+from PIL import Image
+from streamlit_option_menu import option_menu
+import plotly.express as px
+import pandas as pd
+import matplotlib.pyplot as plt
+import requests
+import geopandas as gpd
+# connect to the database
+import mysql.connector
+import mysql.connector
+import sys
+
+import os
+import json
+import pandas as pd
+
+
+#establishing the connection
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="new_password",
+    database="phonepe_pulse"
+)
+
+
+# create a cursor object
+cursor = conn.cursor(buffered=True)
+
+
+
+
+#with st.sidebar:
+SELECT = option_menu(
+    menu_title = None,
+    options = ["About","Home","Basic insights","Contact"],
+    icons =["bar-chart","house","toggles","at"],
+    default_index=2,
+    orientation="horizontal",
+    styles={"container": {"padding": "0!important", "background-color": "white","size":"cover", "width": "100%"},
+        "icon": {"color": "black", "font-size": "20px"},
+        "nav-link": {"font-size": "20px", "text-align": "center", "margin": "-2px", "--hover-color": "#6F36AD"},
+        "nav-link-selected": {"background-color": "#6F36AD"}})
+
+#---------------------Basic Insights -----------------#
+
+
+if SELECT == "Basic insights":
+    st.title("BASIC INSIGHTS")
+    st.write("----")
+    st.subheader("Let's know some basic insights about the data")
+    options = ["--select--",
+               "Top 10 states based on year and amount of transaction",
+               "List 10 states based on type and amount of transaction",
+               "Top 5 Transaction_Type based on Transaction_Amount",
+               "Top 10 Registered-users based on States and District",
+               "Top 10 Districts based on states and Count of transaction",
+               "List 10 Districts based on states and amount of transaction",
+               "List 10 Transaction_Count based on Districts and states",
+               "Top 10 RegisteredUsers based on states and District"]
+    
+               #1cursor.execute("SELECT DISTINCT States, Transaction_Year, SUM(Transaction_Amount) AS Total_Transaction_Amount FROM top_trans GROUP BY States, Transaction_Year ORDER BY Total_Transaction_Amount DESC LIMIT 10")
+
+               
+    select = st.selectbox("Select the option",options)
+    
+    if select == "Top 10 states based on year and amount of transaction":
+        cursor.execute("DESCRIBE top_trans")
+        column_info = cursor.fetchall()
+        print(column_info)  # Check the console for column information
+        
+        cursor.execute("SELECT DISTINCT State, Year, SUM(Transaction_amount) AS Transaction_amount FROM top_trans GROUP BY State,Year ORDER BY Transaction_amount DESC LIMIT 10")
+
+        
+        data = cursor.fetchall()
+        df = pd.DataFrame(data, columns=['State', 'Year', 'Transaction_amount'])
+        
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.write(df)
+        
+        with col2:
+            st.title("Top 10 states and amount of transaction")
+            st.bar_chart(df, x="State", y="Transaction_amount")
+            
+            #2
+            
+    elif select=="List 10 states based on type and amount of transaction":
+        cursor.execute("SELECT DISTINCT State, SUM(Transaction_count) as Total FROM top_trans GROUP BY State ORDER BY Total ASC LIMIT 10");
+        df = pd.DataFrame(cursor.fetchall(),columns=['State','Total_Transaction'])
+        col1,col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("List 10 states based on type and amount of transaction")
+            st.bar_chart(df,x="Total_Transaction",y="State")
+            
+            #3
+            
+    elif select == "Top 5 Transaction_Type based on Transaction_Amount":
+        cursor.execute("SELECT DISTINCT Transaction_type, SUM(Transaction_Amount) AS Amount FROM agg_trans GROUP BY Transaction_type ORDER BY Amount DESC LIMIT 5")
+        df = pd.DataFrame(cursor.fetchall(), columns=['Transaction_type', 'Transaction_Amount'])
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("Top 5 Transaction_type based on Transaction_Amount")
+            st.bar_chart(data=df, y="Transaction_type", x="Transaction_Amount")
+
+            #4
+            
+    elif select=="Top 10 Registered-users based on States and District":
+        cursor.execute("SELECT DISTINCT State, District, SUM(Registered_user) AS Users FROM map_user GROUP BY State, District ORDER BY Users DESC LIMIT 10");
+        df = pd.DataFrame(cursor.fetchall(),columns=['State','District','Registered_user'])
+        col1,col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("Top 10 Registered-users based on States and District")
+            st.bar_chart(data=df,y="State",x="Registered_user")
+            
+            
+            #5
+            
+    elif select=="Top 10 Districts based on states and Count of transaction":
+        cursor.execute("SELECT DISTINCT State,District,SUM(Count) AS Counts FROM map_trans GROUP BY State,District ORDER BY Counts DESC LIMIT 10");
+        df = pd.DataFrame(cursor.fetchall(),columns=['State','District','Count'])
+        col1,col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("Top 10 Districts based on State and Count of transaction")
+            st.bar_chart(data=df,y="State",x="Count")
+            
+            #6
+            
+    elif select=="List 10 Districts based on states and amount of transaction":
+        cursor.execute("SELECT DISTINCT State,Year,SUM(Transaction_amount) AS Amount FROM agg_trans GROUP BY State, Year ORDER BY Amount ASC LIMIT 10");
+        df = pd.DataFrame(cursor.fetchall(),columns=['State','Year','Transaction_amount'])
+        col1,col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("Least 10 Districts based on state and amount of transaction")
+            st.bar_chart(data=df,y="State",x="Transaction_amount")
+            
+            #7
+            
+    elif select=="List 10 Transaction_Count based on Districts and states":
+        cursor.execute("SELECT DISTINCT State, District, SUM(Count) AS Counts FROM map_trans GROUP BY State,District ORDER BY Counts ASC LIMIT 10");
+        df = pd.DataFrame(cursor.fetchall(),columns=['State','District','Count'])
+        col1,col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("List 10 Count based on Districts and states")
+            st.bar_chart(data=df,y="State",x="Count")
+            
+            #8
+             
+    elif select=="Top 10 RegisteredUsers based on states and District":
+        cursor.execute("SELECT DISTINCT State,District, SUM(Registered_user) AS Users FROM map_user GROUP BY State,District ORDER BY Users DESC LIMIT 10");
+        df = pd.DataFrame(cursor.fetchall(),columns = ['State','District','Registered_user'])
+        col1,col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("Top 10 Registered_user based on state and District")
+            st.bar_chart(data=df,y="State",x="Registered_user")
+
+
+
+
+#----------------Home----------------------#
+cursor = conn.cursor()
+
+# execute a SELECT statement
+cursor.execute("SELECT * FROM agg_trans")
+
+import sys
+
+
+
+
+# fetch all rows
+rows = cursor.fetchall()
+#from streamlit_extras.add_vertical_space import add_vertical_space
+
+
+
+if SELECT == "Home":
+    col1,col2,col3 = st.columns(3)
+    col1.image(Image.open("D:/capstone phonepay project/images.png"),width = 200)
+    with col1,col2:
+        st.subheader("PhonePe  is an Indian digital payments and financial technology company headquartered in Bengaluru, Karnataka, India. PhonePe was founded in December 2015, by Sameer Nigam, Rahul Chari and Burzin Engineer. The PhonePe app, based on the Unified Payments Interface (UPI), went live in August 2016. It is owned by Flipkart, a subsidiary of Walmart.")
+        st.download_button("DOWNLOAD THE APP NOW", "https://www.phonepe.com/app-download/")
+    with col1:
+        st.video("D:/capstone phonepay project/upi.mp4")
+        
+    st.subheader(':blue[Registered Users Hotspots - States]')
+    #col2.image(Image.open("D:/capstone phonepay project/phn.png"),width = 50)
+
+
+    Data_Aggregated_Transaction_df= pd.read_csv(r'D:/capstone phonepay project/Data_Aggregated_Transaction_Table.csv')
+    Data_Aggregated_User_Summary_df= pd.read_csv(r'D:/capstone phonepay project/Data_Aggregated_User_Summary_Table.csv')
+    Data_Aggregated_User_df= pd.read_csv(r'D:/capstone phonepay project/Data_Aggregated_User_Table.csv')
+    Scatter_Geo_Dataset =  pd.read_csv(r'D:/capstone phonepay project/Data_Map_Districts_Longitude_Latitude.csv')
+    Coropleth_Dataset =  pd.read_csv(r'D:/capstone phonepay project/Data_Map_IndiaStates_TU.csv')
+    Data_Map_Transaction_df = pd.read_csv(r'D:/capstone phonepay project/Data_Map_Transaction_Table.csv')
+    Data_Map_User_Table= pd.read_csv(r'D:/capstone phonepay project/Data_Map_User_Table.csv')
+    Indian_States= pd.read_csv(r'D:/capstone phonepay project/Longitude_Latitude_State_Table.csv')
+    
+    c1,c2=st.columns(2)
+    with c1:
+        Year = st.selectbox(
+                'Please select the Year',
+                ('2018', '2019', '2020','2021','2022'))
+    with c2:
+        Quarter = st.selectbox(
+                'Please select the Quarter',
+                ('1', '2', '3','4'))
+    year=int(Year)
+    quarter=int(Quarter)
+    
+    Transaction_scatter_districts=Data_Map_Transaction_df.loc[(Data_Map_Transaction_df['Year'] == year ) & (Data_Map_Transaction_df['Quarter']==quarter) ].copy()
+    Transaction_Coropleth_States=Transaction_scatter_districts[Transaction_scatter_districts["State"] == "india"]
+    Transaction_scatter_districts.drop(Transaction_scatter_districts.index[(Transaction_scatter_districts["State"] == "india")],axis=0,inplace=True)
+    
+     # Dynamic Scattergeo Data Generation
+    
+    Transaction_scatter_districts = Transaction_scatter_districts.sort_values(by=['Place_Name'], ascending=False)
+    Scatter_Geo_Dataset = Scatter_Geo_Dataset.sort_values(by=['District'], ascending=False) 
+    Total_Amount=[]
+    for i in Transaction_scatter_districts['Total_Amount']:
+        Total_Amount.append(i)
+    Scatter_Geo_Dataset['Total_Amount']=Total_Amount
+    Total_Transaction=[]
+    for i in Transaction_scatter_districts['Total_Transactions_count']:
+        Total_Transaction.append(i)
+    Scatter_Geo_Dataset['Total_Transactions']=Total_Transaction
+    Scatter_Geo_Dataset['Year_Quarter']=str(year)+'-Q'+str(quarter)
+    # Dynamic Coropleth
+    
+    Coropleth_Dataset = Coropleth_Dataset.sort_values(by=['state'], ascending=False)
+    Transaction_Coropleth_States = Transaction_Coropleth_States.sort_values(by=['Place_Name'], ascending=False)
+    Total_Amount=[]
+    for i in Transaction_Coropleth_States['Total_Amount']:
+        Total_Amount.append(i)
+    Coropleth_Dataset['Total_Amount']=Total_Amount
+    Total_Transaction=[]
+    for i in Transaction_Coropleth_States['Total_Transactions_count']:
+        Total_Transaction.append(i)
+    Coropleth_Dataset['Total_Transactions']=Total_Transaction 
+    
+    
+    
+    
+    #scatter plotting the states codes 
+    Indian_States = Indian_States.sort_values(by=['state'], ascending=False)
+    Indian_States['Registered_Users']=Coropleth_Dataset['Registered_Users']
+    Indian_States['Total_Amount']=Coropleth_Dataset['Total_Amount']
+    Indian_States['Total_Transactions']=Coropleth_Dataset['Total_Transactions']
+    Indian_States['Year_Quarter']=str(year)+'-Q'+str(quarter)
+    fig=px.scatter_geo(Indian_States,
+                        lon=Indian_States['Longitude'],
+                        lat=Indian_States['Latitude'],                                
+                        text = Indian_States['code'], #It will display district names on map
+                        hover_name="state", 
+                        hover_data=['Total_Amount',"Total_Transactions","Year_Quarter"],
+                        )
+    fig.update_traces(marker=dict(color="white" ,size=0.3))
+    fig.update_geos(fitbounds="locations", visible=False,)
+    # scatter plotting districts
+    Scatter_Geo_Dataset['col']=Scatter_Geo_Dataset['Total_Transactions']
+    fig1=px.scatter_geo(Scatter_Geo_Dataset,
+                        lon=Scatter_Geo_Dataset['Longitude'],
+                        lat=Scatter_Geo_Dataset['Latitude'],
+                        color=Scatter_Geo_Dataset['col'],
+                        size=Scatter_Geo_Dataset['Total_Transactions'],     
+                    #text = Scatter_Geo_Dataset['District'], #It will display district names on map
+                        hover_name="District", 
+                        hover_data=["State", "Total_Amount","Total_Transactions","Year_Quarter"],
+                        title='District',
+                        size_max=22)
+    
+    fig1.update_traces(marker=dict(color="rebeccapurple" ,line_width=1))    #rebeccapurple
+#coropleth mapping india
+    fig_ch = px.choropleth(
+                        Coropleth_Dataset,
+                        geojson="https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson",
+                        featureidkey='properties.ST_NM',                
+                        locations='state',
+                        color="Total_Transactions",                                       
+                        )
+    fig_ch.update_geos(fitbounds="locations", visible=False,)
+#combining districts states and coropleth
+    fig_ch.add_trace( fig.data[0])
+    fig_ch.add_trace(fig1.data[0])
+    st.write("### **:blue[PhonePe India Map]**")
+    colT1,colT2 = st.columns([6,4])
+    with colT1:
+        st.plotly_chart(fig_ch, use_container_width=True)
+    with colT2:
+        st.info(
+        """
+        Details of Map:
+        - The darkness of the state color represents the total transactions
+        - The Size of the Circles represents the total transactions dictrict wise
+        - The bigger the Circle the higher the transactions
+        - Hover data will show the details like Total transactions, Total amount
+        """
+        )
+        st.info(
+        """
+        Important Observations:
+        - User can observe Transactions of PhonePe in both statewide and Districtwide.
+        - We can clearly see the states with highest transactions in the given year and quarter
+        - We get basic idea about transactions district wide
+        """
+        )
+# -----------------------------------------------FIGURE2 HIDDEN BARGRAPH------------------------------------------------------------------------
+    Coropleth_Dataset = Coropleth_Dataset.sort_values(by=['Total_Transactions'])
+    fig = px.bar(Coropleth_Dataset, x='state', y='Total_Transactions',title=str(year)+" Quarter-"+str(quarter))
+    with st.expander("See Bar graph for the same data"):
+        st.plotly_chart(fig, use_container_width=True)
+        st.info('**:blue[The above bar graph showing the increasing order of PhonePe Transactions according to the states of India, Here we can observe the top states with highest Transaction by looking at graph]**')
+
+
+#----------------About-----------------------#
+
+if SELECT == "About":
+    col1,col2 = st.columns(2)
+    with col1:
+        st.video("D:/capstone phonepay project/vid.mp4")
+    with col2:
+        st.image(Image.open("D:/capstone phonepay project/PhonePe_Logo.jpg"),width = 500)
+        st.write("---")
+        st.subheader("The Indian digital payments story has truly captured the world's imagination."
+                 " From the largest towns to the remotest villages, there is a payments revolution being driven by the penetration of mobile phones, mobile internet and states-of-the-art payments infrastructure built as Public Goods championed by the central bank and the government."
+                 " Founded in December 2015, PhonePe has been a strong beneficiary of the API driven digitisation of payments in India. When we started, we were constantly looking for granular and definitive data sources on digital payments in India. "
+                 "PhonePe Pulse is our way of giving back to the digital payments ecosystem.")
+    st.write("---")
+    col1,col2 = st.columns(2)
+    with col1:
+        st.title("THE BEAT OF PHONEPE")
+        st.write("---")
+        st.subheader("Phonepe became a leading digital payments company")
+        st.image(Image.open("D:/capstone phonepay project/top.jpeg"),width = 400)
+        with open("D:/capstone phonepay project/annual report.pdf","rb") as f:
+            data = f.read()
+        st.download_button("DOWNLOAD REPORT",data,file_name="annual report.pdf")
+    with col2:
+        st.image(Image.open("D:/capstone phonepay project/report.jpeg"),width = 200)
+
+
+#----------------------Contact---------------#
+
+
+
+
+if SELECT == "Contact":
+    st.subheader("Kesavan sekar")
+    name = "Kesavan sekar"
+    mail = (f'{"Mail :"}  {"harishu02893@gmail.com"}')
+    description = "An Aspiring DATA-SCIENTIST..!"
+    social_media = {"GITHUB": "https://github.com/harish02893"}
+    
+    col1, col2, col3 = st.columns(3)
+    col1.image(Image.open("D:/capstone phonepay project/136705305.png"), width=180)
+    with col2:
+        st.title('Phonepe Pulse data visualisation')
+        st.write("The goal of this project is to extract data from the Phonepe pulse Github repository, transform and clean the data, insert it into a MySQL database, and create a live geo visualization dashboard using Streamlit and Plotly in Python. The dashboard will display the data in an interactive and visually appealing manner, with at least 10 different dropdown options for users to select different facts and figures to display. The solution must be secure, efficient, and user-friendly, providing valuable insights and information about the data in the Phonepe pulse Github repository.")
+        st.write("---")
+        st.subheader(mail)
+    st.write("#")
+    cols = st.columns(len(social_media))
+    for index, (platform, link) in enumerate(social_media.items()):
+        cols[index].write(f"[{platform}]({link})")
+
+
+
